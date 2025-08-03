17911c5ca343f11cda064a1b2436e2c9ab809cc6
thmastin <thmastin@gmail.com>
2025-08-03 14:41:50 -0400

    Check for file type

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -45,7 +45,10 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 	defer file.Close()
 
-	mediaType := header.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusExpectationFailed, "File is not of type jpeg or png", err)
+	}
 
 	videoMetadata, err := cfg.db.GetVideo(videoID)
 	if err != nil {
