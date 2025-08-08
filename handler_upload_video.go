f3d1973302430758fa24a9bc970de88531008fcb
thmastin <thmastin@gmail.com>
2025-08-08 10:14:49 -0400

    Start implementing video uploading

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,6 +2,28 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("VideoID")
+	videoID, err := uuid.Parse(videoIDString)
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couludn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	const maxMemory = 1 << 30
+	r.ParseMultipartForm(maxMemory)
+
+}
