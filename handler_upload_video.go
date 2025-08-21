24c1ee9ef9f8f6ddd87a134006324900f88930ba
thmastin <thmastin@gmail.com>
2025-08-20 23:14:19 -0400

    Add function to create fast start version of video

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -169,3 +169,13 @@ func getVideoAspectRatio(filepath string) (string, error) {
 	}
 	return "other", nil
 }
+
+func processVideoForFastStart(filepath string) (string, error) {
+	output := filepath + ".processing"
+	cmd := exec.Command("ffmpeg", "-i", filepath, "-c", "coppy", "-movflags", "faststart", "-f", "mp4", output)
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	return output, nil
+}
